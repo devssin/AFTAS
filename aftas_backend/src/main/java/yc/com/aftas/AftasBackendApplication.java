5c694a93309776ba0dbc125ab600cc945bee69b0
devssin <yaaynealhayate@positeams.com>
2023-12-10 22:07:58 +0100

    initial commit

---
+++ aftas_backend/src/main/java/yc/com/aftas/AftasBackendApplication.java
@@ -0,0 +1,13 @@
+package yc.com.aftas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AftasBackendApplication.class, args);
+	}
+
+}
---
+++ aftas_backend/src/test/java/yc/com/aftas/AftasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package yc.com.aftas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
